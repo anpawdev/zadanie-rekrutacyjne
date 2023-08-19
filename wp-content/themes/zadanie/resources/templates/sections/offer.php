da6f46d1bd84e927d1d106cf9a49c33e11a225f5
AniaPawlik <a.pawlik@grafiduo.pl>
2023-08-19 15:57:59 +0200

    + offer

--- wp-content/themes/zadanie/resources/templates/sections/offer.php
+++ wp-content/themes/zadanie/resources/templates/sections/offer.php
@@ -4,7 +4,7 @@
  * @package 
  */
 ?>
-<section class="offer pt-lg-10 pt-5">
+<section class="offer pt-lg-10 pb-12 pt-5">
   <div class="container">
     <div class="row d-md-flex d-block">
       <div class="offset-md-1 col-md-7">
@@ -14,28 +14,32 @@
       </div>
     </div>
     <div class="row">
-      <?php if (have_rows('box_repeater')) : ?>
-        <?php while (have_rows('box_repeater')) : the_row(); ?>
-          <div class="col-md-3 col-12 bg-white">
-            <figure>
-              <img src="<?php the_sub_field('box_icon'); ?>" alt="icon" />
-            </figure>
+      <div class="col-12 offer__wrapper mt-lg-7 mt-5">
+        <?php if (have_rows('box_repeater')) : ?>
+          <?php while (have_rows('box_repeater')) : the_row(); ?>
+            <div class="offer__box bg-white d-flex flex-column justify-content-between">
+              <figure>
+                <img src="<?php the_sub_field('box_icon'); ?>" alt="icon" />
+              </figure>
 
-            <h4><?php the_sub_field('box_title'); ?></h4>
-            <p><?php the_sub_field('box_description'); ?></p>
+                <h4><?php the_sub_field('box_title'); ?></h4>
+                <p><?php the_sub_field('box_description'); ?></p>
 
-            <?php
-            $link = get_sub_field('link_cta');
-            if ($link) :
-              $link_url = $link['url'];
-              $link_title = $link['title'];
-              $link_target = $link['target'] ? $link['target'] : '_self';
-            ?>
-              <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
-            <?php endif; ?>
-          </div>
-        <?php endwhile; ?>
-      <?php endif; ?>
+                <?php
+                $link = get_sub_field('link_cta');
+                if ($link) :
+                  $link_url = $link['url'];
+                  $link_title = $link['title'];
+                  $link_target = $link['target'] ? $link['target'] : '_self';
+                ?>
+                  <a class="position-relative align-self-start" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?>
+                    <img src="<?php echo esc_url(get_template_directory_uri()); ?>/public/images/icons/arrow-green.svg" alt="arrow" class="ml-2 arrow-animate">
+                  </a>
+                <?php endif; ?>
+                </div>
+          <?php endwhile; ?>
+        <?php endif; ?>
+      </div>
     </div>
   </div>
 </section>
